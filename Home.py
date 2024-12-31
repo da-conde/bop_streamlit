230230cf2bf13241d565f9445bf9cec47cec9d90
Daniel Conde <danielconde@MacBookPro.fritz.box>
2024-12-31 11:09:54 +0100

    f

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 from streamlit_pandas_profiling import st_profile_report 
-#from ydata_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 #import matplotlib.pyplot as plt
 
 import numpy as np
